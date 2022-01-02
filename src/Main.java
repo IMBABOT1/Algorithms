abfe58c301938873579368689a45708193965668
IMBABOT1 <zlovred12@gmail.com>
2022-01-03 01:16:13 +0300

    ImprovedBubbleSort

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         ImprovedBubbleSort bs = new ImprovedBubbleSort();
 
-        int[] arr = {1,-58,13587, 8, 3, 2, 135, 98, 6};
+        int[] arr = {16, 14, 5, 6, 8};
         bs.bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
 
--- src/Sorting/ImprovedBubbleSort.java
+++ src/Sorting/ImprovedBubbleSort.java
@@ -4,12 +4,17 @@ public class ImprovedBubbleSort {
 
     public void bubbleSort(int[] arr){
         for (int i = 0; i < arr.length - 1 ; i++) {
+            boolean isSwap = false;
             for (int j = 0; j < arr.length - 1 - i ; j++) {
                 if (arr[j] > arr[j + 1]){
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    isSwap = true;
                 }
+            }
+            if (isSwap == false){
+                break;
             }
         }
     }
