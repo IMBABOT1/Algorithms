ffe2ae3a1ed8a988d8e6ade6e65b72686a3a3466
IMBABOT1 <zlovred12@gmail.com>
2022-01-04 08:29:37 +0300

    shiftLeft

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
 
         int[] arr = {10,20,30,40,50,60,70,80,90,100};
-        ShiftElements.shiftRight(arr);
+        ShiftElements.shiftLeft(arr);
         System.out.println(Arrays.toString(arr));
 
     }
--- src/ShiftElements.java
+++ src/ShiftElements.java
@@ -1,11 +1,25 @@
+import java.util.Arrays;
+
 public class ShiftElements {
 
 
     public static void shiftRight(int[] arr){
         int temp = arr[arr.length - 1];
+
         for (int i = arr.length - 2; i >= 0 ; i--) {
             arr[i + 1] = arr[i];
         }
         arr[0] = temp;
+    }
+
+    public static void shiftLeft(int[] arr){
+        int temp = arr[0];
+
+        for (int i = 0; i < arr.length -1 ; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        arr[arr.length - 1] = temp;
+
     }
 }
