3fd580b1134a7158391c776534b49d161dbf072c
IMBABOT1 <zlovred12@gmail.com>
2022-01-08 14:17:08 +0300

    insertionSort

--- src/Structure/Main.java
+++ src/Structure/Main.java
@@ -2,14 +2,19 @@ package Structure;
 
 
 import Algorithms.BinarySearch;
+import Sorting.InsertionSort;
 import Structure.LinkedList.CircularList.CircularList;
+
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         int[]arr = {1, 5, 8, 10, 111, 536};
 
-        System.out.println(BinarySearch.binarySearch(arr, 536));
+        InsertionSort.insertionSort(arr);
+
+        System.out.println(Arrays.toString(arr));
 
     }
 }
