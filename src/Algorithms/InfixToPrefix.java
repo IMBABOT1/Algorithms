b2f91266adf9d134eb927c8b9f7d08f074d9ef19
IMBABOT1 <zlovred12@gmail.com>
2022-01-18 16:04:28 +0300

    temp

--- src/Algorithms/InfixToPrefix.java
+++ src/Algorithms/InfixToPrefix.java
@@ -10,8 +10,9 @@ public class InfixToPrefix {
         Stack<Character> stack = new Stack<>();
         StringBuilder temp = new StringBuilder(infix).reverse();
         String[] reverse = temp.toString().split("");
+        String res = "";
+        StringBuilder sb = new StringBuilder();
 
-        System.out.println(Arrays.toString(reverse));
         for (int i = 0; i < reverse.length ; i++) {
             if (reverse[i].equals(")")){
                 reverse[i] = "(";
@@ -19,9 +20,22 @@ public class InfixToPrefix {
                 reverse[i] = ")";
             }
         }
-        System.out.println(Arrays.toString(reverse));
+        res = String.join(",", reverse);
+        res = res.replace(",", "");
+        System.out.println(res);
 
-        return "";
+        for (int i = 0; i < res.length() ; i++) {
+            if ((res.charAt(i) >= 'a' && res.charAt(i) <= 'z')||
+                    (res.charAt(i)>= 'A' && res.charAt(i) <= 'Z')){
+                sb.append(res.charAt(i));
+            }
+        }
+
+        System.out.println(sb);
+
+
+
+        return sb.toString();
     }
 
 }
