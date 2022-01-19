a7e130cd27bfe52dc2fd60bf049d5262a10a9846
IMBABOT1 <zlovred12@gmail.com>
2022-01-19 14:59:32 +0300

    LinkedQueue

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
-import Structure.Stack.LinkedStack;
+import Structure.Queue.Queue.LinkedQueue;
 
-import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
-        LinkedStack<Integer> linkedStack = new LinkedStack<>();
-        System.out.println(linkedStack);
-        linkedStack.push(123);
-        linkedStack.push(321);
-        System.out.println(linkedStack.pop());
-        System.out.println(linkedStack.pop());
-        System.out.println(linkedStack);
+        LinkedQueue<Integer> linkedQueue = new LinkedQueue<>();
+
+        System.out.println(linkedQueue.isEmpty());
+        linkedQueue.enqueue(132);
+        linkedQueue.enqueue(321);
+        System.out.println(linkedQueue);
     }
 }
--- src/Structure/Queue/Queue/LinkedQueue.java
+++ src/Structure/Queue/Queue/LinkedQueue.java
@@ -31,4 +31,81 @@ public class LinkedQueue<T> {
             return Objects.equals(this.data, node.data);
         }
     }
+
+    private Node<T> rear;
+    private Node<T> front;
+
+    public LinkedQueue(){
+        rear = null;
+        front = null;
+    }
+
+    public boolean isEmpty(){
+        return rear == null && front == null;
+    }
+
+    public boolean exist(T element) {
+        Node<T> node = new Node<>(element);
+        Node<T> temp = front;
+        boolean exist = false;
+
+        while (temp != null){
+            if (temp == node){
+                exist = true;
+            }
+            temp = temp.next;
+        }
+        return exist;
+    }
+
+    public void enqueue(T element){
+        Node<T> node = new Node<>(element);
+        if (isEmpty()){
+            rear = node;
+            front = node;
+        }else {
+            rear.next = node;
+            rear = node;
+        }
+    }
+
+    public T dequeue(){
+        Node<T> temp = null;
+        if (isEmpty()){
+            throw new RuntimeException("Queue is empty");
+        }else if (front == rear){
+            temp = front;
+            front = null;
+            rear = null;
+            return temp.data;
+        }else {
+            temp = front;
+            front = front.next;
+            return temp.data;
+        }
+    }
+
+    public int count(){
+        int count = 0;
+        Node<T> temp = front;
+        while (temp != null){
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty()) return "[]";
+        Node<T> temp = front;
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        while (temp != null){
+            sb.append(temp);
+            temp = temp.next;
+            sb.append((temp == null) ? "]" : ", ");
+        }
+        return sb.toString();
+    }
 }
