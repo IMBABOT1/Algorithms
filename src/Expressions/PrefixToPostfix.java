04a15b087e4d2b1537afe229e9126b07e4093d2b
IMBABOT1 <zlovred12@gmail.com>
2022-01-19 11:49:06 +0300

    PrefixToPostfix

---
+++ src/Expressions/PrefixToPostfix.java
@@ -0,0 +1,37 @@
+package Expressions;
+
+import java.util.Stack;
+
+public class PrefixToPostfix {
+
+
+    private static boolean isOperand(char c){
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c<= 'Z')){
+            return true;
+        }
+        return false;
+    }
+
+    public static String prefixToPostfix(String prefix){
+        Stack<String> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = prefix.length() - 1; i >= 0 ; i--) {
+            if (isOperand(prefix.charAt(i))){
+                result.append(prefix.charAt(i));
+                stack.push(result.toString());
+                result.setLength(0);
+            }else {
+                String op1 = stack.peek();
+                stack.pop();
+                String op2 = stack.peek();
+                stack.pop();
+                String exp = op1 + op2 + prefix.charAt(i);
+                stack.push(exp);
+            }
+        }
+
+        return stack.peek();
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import Expressions.PostfixToPrefix;
+import Expressions.PrefixToPostfix;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(PostfixToPrefix.postfixToPrefix("ab+"));
+        System.out.println(PrefixToPostfix.prefixToPostfix("/*-*abc^d^efg"));
     }
 }
